3720a3fdc697e70034805cafb646f3793d0b3163
Tharunika21 <2326ka55@kgcas.com>
2025-06-05 15:10:15 +0530

    Add files via upload

---
+++ java.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class java {
+    public static void main(String[] args) {
+    Scanner in=new Scanner(System.in);
+    System.out.println("enter thr simple intrest");
+    int p=1000,r=5,time=2;
+    int s=p*r*time/100;
+    System.out.println(s);
+        
+       
+    }
+}
